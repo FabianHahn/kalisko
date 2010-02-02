c85ddd4292629eacd4439fb62543fc5c0aaebe0f
Fabian Hahn <fabian@kalisko.org>
2010-02-02 21:24:28 +0100

    * Wrapped the remaining core module XCall functions in the lua_core API  * lua_core version bump to 0.1.1

--- src/modules/lua_core/KaliskoModule.lua
+++ src/modules/lua_core/KaliskoModule.lua
@@ -22,4 +22,54 @@ function KaliskoModule:isLoaded()
 	return xcs.loaded > 0
 end
 
+function KaliskoModule:getAuthor()
+	xcall = invokeXCall('xcall = { function = getModuleAuthor }; module = ' .. self.name)
+	xcs = parseStore(xcall)
+	return xcs.author
+end
+
+function KaliskoModule:getDescription()
+	xcall = invokeXCall('xcall = { function = getModuleDescription }; module = ' .. self.name)
+	xcs = parseStore(xcall)
+	return xcs.description
+end
+
+function KaliskoModule:getVersion()
+	xcall = invokeXCall('xcall = { function = getModuleVersion }; module = ' .. self.name)
+	xcs = parseStore(xcall)
 	
+	if xcs.version ~= nil then
+		return xcs.version.string
+	else
+		return nil
+	end
+end
+
+function KaliskoModule:getBcVersion()
+	xcall = invokeXCall('xcall = { function = getModuleBcVersion }; module = ' .. self.name)
+	xcs = parseStore(xcall)
+	
+	if xcs.bcversion ~= nil then
+		return xcs.bcversion.string
+	else
+		return nil
+	end
+end
+
+function KaliskoModule:getReferenceCount()
+	xcall = invokeXCall('xcall = { function = getModuleReferenceCount }; module = ' .. self.name)
+	xcs = parseStore(xcall)
+	return xcs.reference_count
+end
+
+function KaliskoModule:request()
+	xcall = invokeXCall('xcall = { function = requestModule }; module = ' .. self.name)
+	xcs = parseStore(xcall)
+	return xcs.success > 0
+end
+
+function KaliskoModule:revoke()
+	xcall = invokeXCall('xcall = { function = revokeModule }; module = ' .. self.name)
+	xcs = parseStore(xcall)
+	return xcs.success > 0
+end
--- src/modules/lua_core/lua_core.c
+++ src/modules/lua_core/lua_core.c
@@ -30,7 +30,7 @@
 MODULE_NAME("lua_core");
 MODULE_AUTHOR("The Kalisko Team");
 MODULE_DESCRIPTION("The lua_core module provides a Lua API to the Kalisko core");
-MODULE_VERSION(0, 1, 0);
+MODULE_VERSION(0, 1, 1);
 MODULE_BCVERSION(0, 1, 0);
 MODULE_DEPENDS(MODULE_DEPENDENCY("xcall_core", 0, 2, 1), MODULE_DEPENDENCY("lang_lua", 0, 3, 1));
 

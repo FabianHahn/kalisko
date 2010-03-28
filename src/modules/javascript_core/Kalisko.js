71424f68ce9198cadf53e263ee57fa7f48530be0
Gregor Wegberg <gregor@kalisko.org>
2010-03-28 03:02:12 +0200

    javascript_core Kalisko.js is now much nicer to read & version bump

--- src/modules/javascript_core/Kalisko.js
+++ src/modules/javascript_core/Kalisko.js
@@ -1,32 +1,56 @@
 var kalisko = {
 	logError: function(message)
 	{
-		var ret = xcall.invoke(
-			'message = "' + message + '", xcall = { function = logError }');
+		var call = {
+			message: message,
+			xcall: {
+				function: "logError"
+			}
+		};
+		
+		var ret = xcall.invoke(call);
 
 		return ret.success > 0;
 	},
 
 	logWarning: function(message)
 	{
-		var ret = xcall.invoke(
-			'message = "' + message + '", xcall = { function = logWarning }');
+		var call = {
+			message: message,
+			xcall: {
+				function: "logWarning"
+			}
+		};
+		
+		var ret = xcall.invoke(call);
 
 		return ret.success > 0;
 	},
 
 	logInfo: function(message)
 	{
-		var ret = xcall.invoke(
-			'message = "' + message + '", xcall = { function = logInfo }');
+		var call = {
+			message: message,
+			xcall: {
+				function: "logInfo"
+			}
+		};
+		
+		var ret = xcall.invoke(call);
 
 		return ret.success > 0;
 	},
 
 	logDebug: function(message)
 	{
-		var ret = xcall.invoke(
-			'message = "' + message + '", xcall = { function = logDebug }');
+		var call = {
+			message: message,
+			xcall: {
+				function: "logDeug"
+			}
+		};
+		
+		var ret = xcall.invoke(call);
 
 		return ret.success > 0;
 	}

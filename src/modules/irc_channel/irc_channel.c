11fe0705ea8b91e0ed0d2756180905983664eb0e
Fabian Hahn <fabian@kalisko.org>
2010-04-28 13:47:55 +0200

    * added implementation for enableChannelTracking and disableChannelTracking in irc_channel  * added irc_line hook listener to irc_channel

--- src/modules/irc_channel/irc_channel.c
+++ src/modules/irc_channel/irc_channel.c
@@ -25,6 +25,7 @@
 #include "hooks.h"
 #include "log.h"
 #include "types.h"
+#include "memory_alloc.h"
 #include "modules/irc/irc.h"
 #include "api.h"
 #include "irc_channel.h"
@@ -36,14 +37,26 @@ MODULE_VERSION(0, 1, 0);
 MODULE_BCVERSION(0, 1, 0);
 MODULE_DEPENDS(MODULE_DEPENDENCY("irc", 0, 3, 2));
 
+HOOK_LISTENER(irc_line);
+
 static GHashTable *tracked;
 
 MODULE_INIT
 {
+	tracked = g_hash_table_new(NULL, NULL);
+
+	HOOK_ATTACH(irc_line, irc_line);
+
 	return true;
 }
 
 MODULE_FINALIZE
+{
+	HOOK_DETACH(irc_line, irc_line);
+	g_hash_table_destroy(tracked);
+}
+
+HOOK_LISTENER(irc_line)
 {
 
 }
@@ -56,7 +69,18 @@ MODULE_FINALIZE
  */
 API bool enableChannelTracking(IrcConnection *irc)
 {
+	IrcChannelTracker *tracker;
 
+	if((tracker = g_hash_table_lookup(tracked, irc)) != NULL) {
+		LOG_WARNING("Trying to enable channel tracking for already tracked IRC connection %d, aborting", irc->socket->fd);
+		return false;
+	}
+
+	tracker = ALLOCATE_OBJECT(IrcChannelTracker);
+	tracker->irc = irc;
+	tracker->channels = g_hash_table_new(&g_str_hash, &g_str_equal);
+
+	return true;
 }
 
 /**
@@ -66,5 +90,13 @@ API bool enableChannelTracking(IrcConnection *irc)
  */
 API void disableChannelTracking(IrcConnection *irc)
 {
+	IrcChannelTracker *tracker;
 
+	if((tracker = g_hash_table_lookup(tracked, irc)) == NULL) {
+		LOG_WARNING("Trying to disable channel tracking for untracked IRC connection %d, aborting", irc->socket->fd);
+		return;
+	}
+
+	g_hash_table_destroy(tracker->channels);
+	free(tracker);
 }

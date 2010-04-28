4a44e7e63d8c06e7dbb3b2207603aee0715d47b6
Fabian Hahn <fabian@kalisko.org>
2010-04-28 14:59:23 +0200

    irc_channel:  * version bump to 0.1.1  * implemented irc_line hook listener  * added proper tracker freeing on module finalization

--- src/modules/irc_channel/irc_channel.c
+++ src/modules/irc_channel/irc_channel.c
@@ -18,7 +18,6 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-
 #include <glib.h>
 
 #include "dll.h"
@@ -27,17 +26,20 @@
 #include "types.h"
 #include "memory_alloc.h"
 #include "modules/irc/irc.h"
+#include "modules/irc_parser/irc_parser.h"
 #include "api.h"
 #include "irc_channel.h"
 
 MODULE_NAME("irc_channel");
 MODULE_AUTHOR("The Kalisko team");
 MODULE_DESCRIPTION("The IRC channel module keeps track of channel joins and leaves as well as of their users");
-MODULE_VERSION(0, 1, 0);
+MODULE_VERSION(0, 1, 1);
 MODULE_BCVERSION(0, 1, 0);
-MODULE_DEPENDS(MODULE_DEPENDENCY("irc", 0, 3, 2));
+MODULE_DEPENDS(MODULE_DEPENDENCY("irc", 0, 3, 2), MODULE_DEPENDENCY("irc_parser", 0, 1, 0));
 
 HOOK_LISTENER(irc_line);
+static void freeIrcChannel(void *channel_p);
+static bool freeIrcChannelTracker(void *key_p, void *tracker_p, void *data);
 
 static GHashTable *tracked;
 
@@ -53,12 +55,46 @@ MODULE_INIT
 MODULE_FINALIZE
 {
 	HOOK_DETACH(irc_line, irc_line);
+	g_hash_table_foreach_remove(tracked, &freeIrcChannelTracker, NULL);
 	g_hash_table_destroy(tracked);
 }
 
 HOOK_LISTENER(irc_line)
 {
+	IrcConnection *irc = HOOK_ARG(IrcConnection *);
+	IrcMessage *message = HOOK_ARG(IrcMessage *);
 
+	IrcUserMask *mask = $(IrcUserMask *, irc_parser, parseIrcUserMask)(message->prefix);
+	if(mask == NULL) {
+		return;
+	}
+
+	IrcChannelTracker *tracker;
+	IrcChannel *channel;
+	if((tracker = g_hash_table_lookup(tracked, irc)) != NULL) {
+		if(g_strcmp0(message->command, "JOIN") == 0 && message->params_count >= 1) {
+			if(g_strcmp0(irc->nick, mask->nick) == 0) { // Its ourselves!
+				// Construct new channel
+				channel = ALLOCATE_OBJECT(IrcChannel);
+				channel->name = strdup(message->params[0]);
+				channel->tracker = tracker;
+				g_hash_table_insert(tracker->channels, channel->name, channel); // add it to channels table
+
+				LOG_DEBUG("Joined channel %s on IRC conection %d", channel->name, irc->socket->fd);
+			}
+		} else if(g_strcmp0(message->command, "PART") == 0 && message->params_count >= 1) {
+			if(g_strcmp0(irc->nick, mask->nick) == 0) { // Its ourselves!
+				if((channel = g_hash_table_lookup(tracker->channels, message->params[0])) != NULL) {
+					free(channel->name);
+					free(channel);
+
+					LOG_DEBUG("Left channel %s on IRC connection %d", message->params[0], irc->socket->fd);
+				}
+			}
+		}
+
+		free(mask);
+	}
 }
 
 /**
@@ -78,7 +114,7 @@ API bool enableChannelTracking(IrcConnection *irc)
 
 	tracker = ALLOCATE_OBJECT(IrcChannelTracker);
 	tracker->irc = irc;
-	tracker->channels = g_hash_table_new(&g_str_hash, &g_str_equal);
+	tracker->channels = g_hash_table_new_full(&g_str_hash, &g_str_equal, NULL, &freeIrcChannel);
 
 	return true;
 }
@@ -99,4 +135,34 @@ API void disableChannelTracking(IrcConnection *irc)
 
 	g_hash_table_destroy(tracker->channels);
 	free(tracker);
+}
+
+/**
+ * A GDestroyNotify function to free an IRC channel
+ *
+ * @param channel_p			a pointer to the IRC channel to be freed
+ */
+static void freeIrcChannel(void *channel_p)
+{
+	IrcChannel *channel = channel_p;
+
+	free(channel->name);
+	free(channel);
+}
+
+/**
+ * A GHRFunc to free an IRC channel tracker
+ *
+ * @param key_p			unused
+ * @param tracker_p		a pointer to the tracker to free
+ * @param data			unused
+ */
+static bool freeIrcChannelTracker(void *key_p, void *tracker_p, void *data)
+{
+	IrcChannelTracker *tracker = tracker_p;
+
+	g_hash_table_destroy(tracker->channels);
+	free(tracker);
+
+	return true; // always free the tracker in the parent hashtable
 }

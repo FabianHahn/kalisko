aca6815975cd8c021f82703c334884c9d80d75b8
Fabian Hahn <fabian@kalisko.org>
2010-04-28 13:27:37 +0200

    added irc_channel module skeleton

---
+++ src/modules/irc_channel/irc_channel.c
@@ -0,0 +1,70 @@
+/**
+ * @file
+ * <h3>Copyright</h3>
+ * Copyright (c) 2009, Kalisko Project Leaders
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ *     @li Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *     @li Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer
+ *       in the documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+#include <glib.h>
+
+#include "dll.h"
+#include "hooks.h"
+#include "log.h"
+#include "types.h"
+#include "modules/irc/irc.h"
+#include "api.h"
+#include "irc_channel.h"
+
+MODULE_NAME("irc_channel");
+MODULE_AUTHOR("The Kalisko team");
+MODULE_DESCRIPTION("The IRC channel module keeps track of channel joins and leaves as well as of their users");
+MODULE_VERSION(0, 1, 0);
+MODULE_BCVERSION(0, 1, 0);
+MODULE_DEPENDS(MODULE_DEPENDENCY("irc", 0, 3, 2));
+
+static GHashTable *tracked;
+
+MODULE_INIT
+{
+	return true;
+}
+
+MODULE_FINALIZE
+{
+
+}
+
+/**
+ * Enables channel tracking for an IRC connection
+ *
+ * @param irc		the IRC connection to enable tracking for
+ * @result			true if successful
+ */
+API bool enableChannelTracking(IrcConnection *irc)
+{
+
+}
+
+/**
+ * Disables channel tracking for an IRC connection
+ *
+ * @param irc		the IRC connection to disable tracking for
+ */
+API void disableChannelTracking(IrcConnection *irc)
+{
+
+}

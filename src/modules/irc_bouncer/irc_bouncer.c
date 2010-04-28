8fe682cf844063701bd15767452681954d503f90
Fabian Hahn <fabian@kalisko.org>
2010-04-28 13:29:08 +0200

    * fixed irc bouncer module name  * irc_bouncer version bump to 0.1.4

--- src/modules/irc_bouncer/irc_bouncer.c
+++ src/modules/irc_bouncer/irc_bouncer.c
@@ -33,10 +33,10 @@
 #include "api.h"
 
 
-MODULE_NAME("irc_boucer");
+MODULE_NAME("irc_bouncer");
 MODULE_AUTHOR("The Kalisko team");
 MODULE_DESCRIPTION("A simple IRC bouncer using an IRC connection to a single IRC server on a listening port");
-MODULE_VERSION(0, 1, 3);
+MODULE_VERSION(0, 1, 4);
 MODULE_BCVERSION(0, 1, 0);
 MODULE_DEPENDS(MODULE_DEPENDENCY("irc", 0, 2, 7), MODULE_DEPENDENCY("irc_proxy", 0, 1, 1), MODULE_DEPENDENCY("config", 0, 2, 3), MODULE_DEPENDENCY("store", 0, 6, 0));
 

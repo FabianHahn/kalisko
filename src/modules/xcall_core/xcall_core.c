3ec1a761c3234d9a4f5d3b2106691b543236ed59
Fabian Hahn <fabian@kalisko.org>
2010-04-28 16:25:51 +0200

    * added XCall function for getHookStats core API function to xcall_core  * xcall_core version bump to 0.3.6

--- src/modules/xcall_core/xcall_core.c
+++ src/modules/xcall_core/xcall_core.c
@@ -36,10 +36,11 @@
 MODULE_NAME("xcall_core");
 MODULE_AUTHOR("The Kalisko team");
 MODULE_DESCRIPTION("Module which offers an XCall API to the Kalisko Core");
-MODULE_VERSION(0, 3, 5);
+MODULE_VERSION(0, 3, 6);
 MODULE_BCVERSION(0, 3, 2);
 MODULE_DEPENDS(MODULE_DEPENDENCY("xcall", 0, 2, 3), MODULE_DEPENDENCY("store", 0, 6, 0));
 
+static Store *xcall_getHookStats(Store *xcall);
 static Store *xcall_attachLog(Store *xcall);
 static Store *xcall_detachLog(Store *xcall);
 static Store *xcall_getModuleAuthor(Store *xcall);
@@ -72,6 +73,7 @@ MODULE_INIT
 	logExecuting = false;
 	logListeners = g_hash_table_new_full(&g_str_hash, &g_str_equal, NULL, &freeLogListenerEntry);
 
+	$(bool, xcall, addXCallFunction)("getHookStats", &xcall_getHookStats);
 	$(bool, xcall, addXCallFunction)("attachLog", &xcall_attachLog);
 	$(bool, xcall, addXCallFunction)("detachLog", &xcall_detachLog);
 	$(bool, xcall, addXCallFunction)("getModuleAuthor", &xcall_getModuleAuthor);
@@ -96,6 +98,7 @@ MODULE_INIT
 
 MODULE_FINALIZE
 {
+	$(bool, xcall, delXCallFunction)("getHookStats");
 	$(bool, xcall, delXCallFunction)("attachLog");
 	$(bool, xcall, delXCallFunction)("detachLog");
 	$(bool, xcall, delXCallFunction)("getModuleAuthor");
@@ -116,6 +119,42 @@ MODULE_FINALIZE
 	$(bool, xcall, delXCallFunction)("logDebug");
 
 	g_hash_table_destroy(logListeners);
+}
+
+/**
+ * XCallFunction to retrieve hook stats
+ * XCall result:
+ * 	* list hooks 				an array list with (name, listeners) pairs
+ *  * string hooks/name			the name of the hook
+ *  * integer hooks/listeners	the listeners count for the hook
+ *
+ * @param xcall		the xcall as store
+ * @result			a return value as store
+ */
+static Store *xcall_getHookStats(Store *xcall)
+{
+	Store *retstore = $(Store *, store, createStore)();
+	$(bool, store, setStorePath)(retstore, "xcall", $(Store *, store, createStoreArrayValue)(NULL));
+
+	GQueue *stats = $$(GQueue *, getHookStats)();
+	Store *hooksStore = $(Store *, store, createStoreListValue)(NULL);
+
+	for(GList *iter = stats->head; iter != NULL; iter = iter->next) {
+		HookStatsEntry *entry = iter->data;
+
+		// Read data from entry
+		Store *hook = $(Store *, store, createStoreArrayValue)(NULL);
+		$(bool, store, setStorePath)(hook, "name", $(Store *, store, createStoreStringValue)(entry->hook_name));
+		$(bool, store, setStorePath)(hook, "listeners", $(Store *, store, createStoreIntegerValue)(entry->num_listeners));
+
+		// Push to hook store
+		g_queue_push_tail(hooksStore->content.list, hook);
+	}
+
+	$(bool, store, setStorePath)(retstore, "hooks", hooksStore);
+	$$(void, freeHookStats)(stats);
+
+	return retstore;
 }
 
 /**

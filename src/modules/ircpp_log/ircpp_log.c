844d6075574d0570a5a5c4e6e96d96f20ec2435e
Fabian Hahn <fabian@kalisko.org>
2010-05-12 20:44:54 +0200

    * split up ircpp_log IRC proxy plugin log into 4 plugins for each log level  * ircpp_log version bump to 0.2.0

--- src/modules/ircpp_log/ircpp_log.c
+++ src/modules/ircpp_log/ircpp_log.c
@@ -33,30 +33,78 @@
 MODULE_NAME("ircpp_log");
 MODULE_AUTHOR("The Kalisko team");
 MODULE_DESCRIPTION("An IRC proxy plugin that allows log messages to be relayed to IRC proxy clients");
-MODULE_VERSION(0, 1, 1);
-MODULE_BCVERSION(0, 1, 0);
+MODULE_VERSION(0, 2, 0);
+MODULE_BCVERSION(0, 2, 0);
 MODULE_DEPENDS(MODULE_DEPENDENCY("irc_proxy", 0, 1, 13), MODULE_DEPENDENCY("irc_proxy_plugin", 0, 2, 0), MODULE_DEPENDENCY("irc_parser", 0, 1, 1));
 
 HOOK_LISTENER(log);
 static bool initPlugin(IrcProxy *proxy, char *name);
 static void finiPlugin(IrcProxy *proxy, char *name);
-static IrcProxyPlugin plugin;
+static IrcProxyPlugin plugin_debug;
+static IrcProxyPlugin plugin_info;
+static IrcProxyPlugin plugin_warning;
+static IrcProxyPlugin plugin_error;
 
 /**
- * A queue with proxies that have the plugin enabled
+ * A queue with proxies that have the plugin_debug enabled
  */
-static GQueue *proxies;
+static GQueue *proxies_debug;
+
+/**
+ * A queue with proxies that have the plugin_info enabled
+ */
+static GQueue *proxies_info;
+
+/**
+ * A queue with proxies that have the plugin_warning enabled
+ */
+static GQueue *proxies_warning;
+
+/**
+ * A queue with proxies that have the plugin_error enabled
+ */
+static GQueue *proxies_error;
 
 MODULE_INIT
 {
-	proxies = g_queue_new();
+	proxies_debug = g_queue_new();
+	proxies_info = g_queue_new();
+	proxies_warning = g_queue_new();
+	proxies_error = g_queue_new();
 
-	plugin.name = "log";
-	plugin.handlers = g_queue_new();
-	plugin.initialize = &initPlugin;
-	plugin.finalize = &finiPlugin;
+	plugin_debug.name = "log_debug";
+	plugin_debug.handlers = g_queue_new();
+	plugin_debug.initialize = &initPlugin;
+	plugin_debug.finalize = &finiPlugin;
 
-	if(!$(bool, irc_proxy_plugin, addIrcProxyPlugin)(&plugin)) {
+	if(!$(bool, irc_proxy_plugin, addIrcProxyPlugin)(&plugin_debug)) {
+		return false;
+	}
+
+	plugin_info.name = "log_info";
+	plugin_info.handlers = g_queue_new();
+	plugin_info.initialize = &initPlugin;
+	plugin_info.finalize = &finiPlugin;
+
+	if(!$(bool, irc_proxy_plugin, addIrcProxyPlugin)(&plugin_info)) {
+		return false;
+	}
+
+	plugin_warning.name = "log_warning";
+	plugin_warning.handlers = g_queue_new();
+	plugin_warning.initialize = &initPlugin;
+	plugin_warning.finalize = &finiPlugin;
+
+	if(!$(bool, irc_proxy_plugin, addIrcProxyPlugin)(&plugin_warning)) {
+		return false;
+	}
+
+	plugin_error.name = "log_error";
+	plugin_error.handlers = g_queue_new();
+	plugin_error.initialize = &initPlugin;
+	plugin_error.finalize = &finiPlugin;
+
+	if(!$(bool, irc_proxy_plugin, addIrcProxyPlugin)(&plugin_error)) {
 		return false;
 	}
 
@@ -67,11 +115,17 @@ MODULE_INIT
 
 MODULE_FINALIZE
 {
-	g_queue_free(proxies);
+	g_queue_free(proxies_debug);
+	g_queue_free(proxies_info);
+	g_queue_free(proxies_warning);
+	g_queue_free(proxies_error);
 
 	HOOK_DETACH(log, log);
 
-	$(void, irc_proxy_plugin, delIrcProxyPlugin)(&plugin);
+	$(void, irc_proxy_plugin, delIrcProxyPlugin)(&plugin_debug);
+	$(void, irc_proxy_plugin, delIrcProxyPlugin)(&plugin_info);
+	$(void, irc_proxy_plugin, delIrcProxyPlugin)(&plugin_warning);
+	$(void, irc_proxy_plugin, delIrcProxyPlugin)(&plugin_error);
 }
 
 HOOK_LISTENER(log)
@@ -81,18 +135,24 @@ HOOK_LISTENER(log)
 
 	GString *msg = g_string_new("");
 
+	GQueue *proxies;
+
 	switch(type) {
 		case LOG_TYPE_DEBUG:
 			g_string_append_printf(msg, "(%c3debug%c) %s", (char) 3, (char) 0x0f, message);
+			proxies = proxies_debug;
 		break;
 		case LOG_TYPE_INFO:
 			g_string_append_printf(msg, "(%c12info%c) %s", (char) 3, (char) 0x0f, message);
+			proxies = proxies_info;
 		break;
 		case LOG_TYPE_WARNING:
 			g_string_append_printf(msg, "(%c7warning%c) %s", (char) 3, (char) 0x0f, message);
+			proxies = proxies_warning;
 		break;
 		case LOG_TYPE_ERROR:
 			g_string_append_printf(msg, "(%c4error%c) %s", (char) 3, (char) 0x0f, message);
+			proxies = proxies_error;
 		break;
 	}
 
@@ -118,7 +178,17 @@ HOOK_LISTENER(log)
  */
 static bool initPlugin(IrcProxy *proxy, char *name)
 {
-	g_queue_push_head(proxies, proxy);
+	if(g_strcmp0(name, "log_debug") == 0) {
+		g_queue_push_head(proxies_debug, proxy);
+	} else if(g_strcmp0(name, "log_info") == 0) {
+		g_queue_push_head(proxies_info, proxy);
+	} else if(g_strcmp0(name, "log_warning") == 0) {
+		g_queue_push_head(proxies_warning, proxy);
+	} else if(g_strcmp0(name, "log_error") == 0) {
+		g_queue_push_head(proxies_error, proxy);
+	} else {
+		return false;
+	}
 
 	return true;
 }
@@ -131,5 +201,13 @@ static bool initPlugin(IrcProxy *proxy, char *name)
  */
 static void finiPlugin(IrcProxy *proxy, char *name)
 {
-	g_queue_remove(proxies, proxy);
+	if(g_strcmp0(name, "log_debug") == 0) {
+		g_queue_remove(proxies_debug, proxy);
+	} else if(g_strcmp0(name, "log_info") == 0) {
+		g_queue_remove(proxies_info, proxy);
+	} else if(g_strcmp0(name, "log_warning") == 0) {
+		g_queue_remove(proxies_warning, proxy);
+	} else if(g_strcmp0(name, "log_error") == 0) {
+		g_queue_remove(proxies_error, proxy);
+	}
 }

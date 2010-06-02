d528e80c8e80d509411d5b8549c27998cb064abc
Fabian Hahn <fabian@kalisko.org>
2010-06-02 21:07:38 +0200

    * fixed ircpp_lua, lua_console and lua_core dependencies on lang_lua  * ircpp_lua version bump to 0.2.5  * lua_console version bump to 0.1.2  * lua_core version bump to 0.1.10

--- src/modules/ircpp_lua/ircpp_lua.c
+++ src/modules/ircpp_lua/ircpp_lua.c
@@ -35,9 +35,9 @@
 MODULE_NAME("ircpp_lua");
 MODULE_AUTHOR("The Kalisko team");
 MODULE_DESCRIPTION("An IRC proxy plugin that allows proxy clients to evaluate Lua code by sending private messages to a virtual *lua bot");
-MODULE_VERSION(0, 2, 4);
+MODULE_VERSION(0, 2, 5);
 MODULE_BCVERSION(0, 1, 0);
-MODULE_DEPENDS(MODULE_DEPENDENCY("irc_proxy", 0, 3, 0), MODULE_DEPENDENCY("irc_proxy_plugin", 0, 2, 0), MODULE_DEPENDENCY("lang_lua", 0, 4, 1), MODULE_DEPENDENCY("irc_parser", 0, 1, 1), MODULE_DEPENDENCY("string_util", 0, 1, 2));
+MODULE_DEPENDS(MODULE_DEPENDENCY("irc_proxy", 0, 3, 0), MODULE_DEPENDENCY("irc_proxy_plugin", 0, 2, 0), MODULE_DEPENDENCY("lang_lua", 0, 5, 2), MODULE_DEPENDENCY("irc_parser", 0, 1, 1), MODULE_DEPENDENCY("string_util", 0, 1, 2));
 
 HOOK_LISTENER(client_line);
 static bool initPlugin(IrcProxy *proxy, char *name);

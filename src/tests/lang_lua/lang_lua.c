e8c034b1967fd538e1f63382ed2ba2b253f667c6
Fabian Hahn <fabian@kalisko.org>
2010-06-02 20:44:30 +0200

    lang_lua test suite:  * added store2lua and store2lua_fail test cases  * renamed store conversion test case to lua2store  * version bump to 0.3.0

--- src/tests/lang_lua/lang_lua.c
+++ src/tests/lang_lua/lang_lua.c
@@ -33,23 +33,27 @@
 MODULE_NAME("test_lang_lua");
 MODULE_AUTHOR("The Kalisko team");
 MODULE_DESCRIPTION("Test suite for the lang_lua module");
-MODULE_VERSION(0, 2, 1);
-MODULE_BCVERSION(0, 2, 1);
-MODULE_DEPENDS(MODULE_DEPENDENCY("lang_lua", 0, 4, 0), MODULE_DEPENDENCY("xcall", 0, 2, 2), MODULE_DEPENDENCY("store", 0, 5, 3));
+MODULE_VERSION(0, 3, 0);
+MODULE_BCVERSION(0, 3, 0);
+MODULE_DEPENDS(MODULE_DEPENDENCY("lang_lua", 0, 5, 1), MODULE_DEPENDENCY("xcall", 0, 2, 2), MODULE_DEPENDENCY("store", 0, 5, 3));
 
-TEST_CASE(store);
+TEST_CASE(lua2store);
+TEST_CASE(store2lua);
+TEST_CASE(store2lua_fail);
 TEST_CASE(xcall_invoke);
 TEST_CASE(xcall_define);
 TEST_CASE(xcall_define_error);
 
 TEST_SUITE_BEGIN(lang_lua)
-	TEST_CASE_ADD(store);
+	TEST_CASE_ADD(lua2store);
+	TEST_CASE_ADD(store2lua);
+	TEST_CASE_ADD(store2lua_fail);
 	TEST_CASE_ADD(xcall_invoke);
 	TEST_CASE_ADD(xcall_define);
 	TEST_CASE_ADD(xcall_define_error);
 TEST_SUITE_END
 
-TEST_CASE(store)
+TEST_CASE(lua2store)
 {
 	char *check;
 	TEST_ASSERT($(bool, lang_lua, evaluateLua)("store = parseStore('bird = word; array = { key = value; list = (1 1 2 3 5 7 13 21) }')"));
@@ -78,6 +82,60 @@ TEST_CASE(store)
 	check = $(char *, lang_lua, popLuaString)();
 	TEST_ASSERT(g_strcmp0(check, "13") == 0);
 	free(check);
+
+	TEST_PASS;
+}
+
+TEST_CASE(store2lua)
+{
+	TEST_ASSERT($(bool, lang_lua, evaluateLua)("return {int = 17, float = 3.14, string = 'hello world', array = {foo = 'bar'}, list = {1, 1, 2, 3, 5, 8}, nolist = {4, 2, answer = 42}}"));
+
+	Store *parsed;
+
+	TEST_ASSERT((parsed = $(Store *, lang_lua, popLuaStore)()) != NULL);
+
+	Store *ret;
+	TEST_ASSERT((ret = $(Store *, store, getStorePath)(parsed, "int")) != NULL);
+	TEST_ASSERT(ret->type == STORE_INTEGER);
+	TEST_ASSERT(ret->content.integer == 17);
+
+	TEST_ASSERT((ret = $(Store *, store, getStorePath)(parsed, "float")) != NULL);
+	TEST_ASSERT(ret->type == STORE_FLOAT_NUMBER);
+	TEST_ASSERT(ret->content.float_number == 3.14f);
+
+	TEST_ASSERT((ret = $(Store *, store, getStorePath)(parsed, "string")) != NULL);
+	TEST_ASSERT(ret->type == STORE_STRING);
+	TEST_ASSERT(g_strcmp0(ret->content.string, "hello world") == 0);
+
+	TEST_ASSERT((ret = $(Store *, store, getStorePath)(parsed, "array/foo")) != NULL);
+	TEST_ASSERT(ret->type == STORE_STRING);
+	TEST_ASSERT(g_strcmp0(ret->content.string, "bar") == 0);
+
+	TEST_ASSERT((ret = $(Store *, store, getStorePath)(parsed, "list")) != NULL);
+	TEST_ASSERT(ret->type == STORE_LIST);
+
+	TEST_ASSERT((ret = $(Store *, store, getStorePath)(parsed, "list/2")) != NULL);
+	TEST_ASSERT(ret->type == STORE_INTEGER);
+	TEST_ASSERT(ret->content.integer == 2);
+
+	TEST_ASSERT((ret = $(Store *, store, getStorePath)(parsed, "nolist/answer")) != NULL);
+	TEST_ASSERT(ret->type == STORE_INTEGER);
+	TEST_ASSERT(ret->content.integer == 42);
+
+	$(void, store, freeStore)(parsed);
+
+	TEST_PASS;
+}
+
+TEST_CASE(store2lua_fail)
+{
+	// Test non-string values and error cascading
+	TEST_ASSERT($(bool, lang_lua, evaluateLua)("function foo() return 42; end; return {{foo}}"));
+	TEST_ASSERT($(Store *, lang_lua, popLuaStore)() == NULL);
+
+	// Test non-table argument
+	TEST_ASSERT($(bool, lang_lua, evaluateLua)("return 42"));
+	TEST_ASSERT($(Store *, lang_lua, popLuaStore)() == NULL);
 
 	TEST_PASS;
 }

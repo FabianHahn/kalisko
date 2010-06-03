93cc620c292971719681322a6309f5d40af38d49
Fabian Hahn <fabian@kalisko.org>
2010-06-03 17:14:30 +0200

    * added executeShellCommandArgs XCall function to xcall_exec  * xcall_exec version bump to 0.2.0

--- src/modules/xcall_exec/xcall_exec.c
+++ src/modules/xcall_exec/xcall_exec.c
@@ -32,11 +32,12 @@
 #include "api.h"
 
 static Store *xcall_executeShellCommand(Store *xcall);
+static Store *xcall_executeShellCommandArgs(Store *xcall);
 
 MODULE_NAME("xcall_exec");
 MODULE_AUTHOR("The Kalisko team");
 MODULE_DESCRIPTION("XCall module for exec");
-MODULE_VERSION(0, 1, 0);
+MODULE_VERSION(0, 2, 0);
 MODULE_BCVERSION(0, 1, 0);
 MODULE_DEPENDS(MODULE_DEPENDENCY("exec", 0, 1, 0), MODULE_DEPENDENCY("store", 0, 6, 4), MODULE_DEPENDENCY("xcall", 0, 2, 6));
 
@@ -46,12 +47,17 @@ MODULE_INIT
 		return false;
 	}
 
+	if(!$(bool, xcall, addXCallFunction)("executeShellCommandArgs", &xcall_executeShellCommandArgs)) {
+		return false;
+	}
+
 	return true;
 }
 
 MODULE_FINALIZE
 {
 	$(bool, xcall, delXCallFunction)("executeShellCommand");
+	$(bool, xcall, delXCallFunction)("executeShellCommandArgs");
 }
 
 /**
@@ -77,6 +83,57 @@ static Store *xcall_executeShellCommand(Store *xcall)
 
 	GString *output = $(GString *, exec, executeShellCommand)(command->content.string);
 	$(bool, store, setStorePath)(ret, "output", $(Store *, store, createStoreStringValue)(output->str));
+
+	return ret;
+}
+
+/**
+ * XCallFunction to execute a shell command by a list of arguments
+ * XCall parameters:
+ *  * list args 		a string list of arguments
+ * XCall result:
+ * 	* string output		the output of the executed command
+ *
+ * @param xcall		the xcall as store
+ * @result			a return value as store
+ */
+static Store *xcall_executeShellCommandArgs(Store *xcall)
+{
+	Store *ret = $(Store *, store, createStore)();
+	Store *argsp;
+
+	if((argsp = $(Store *, store, getStorePath)(xcall, "args")) == NULL || argsp->type != STORE_LIST) {
+		$(bool, store, setStorePath)(ret, "xcall", $(Store *, store, createStore)());
+		$(bool, store, setStorePath)(ret, "xcall/error", $(Store *, store, createStoreStringValue)("Failed to read mandatory list parameter 'args'"));
+		return ret;
+	}
+
+	int length = g_queue_get_length(argsp->content.list);
+	char **args = ALLOCATE_OBJECTS(char *, length + 1);
+	memset(args, 0, sizeof(char *) * (length + 1));
+
+	int i = 0;
+
+	for(GList *iter = argsp->content.list->head; iter != NULL; iter = iter->next, i++) {
+		Store *entry = iter->data;
+
+		if(entry->type != STORE_STRING) {
+			g_strfreev(args);
+			GString *error = g_string_new("");
+			g_string_append_printf(error, "args list parameter %d is not of type string", i);
+			$(bool, store, setStorePath)(ret, "xcall", $(Store *, store, createStore)());
+			$(bool, store, setStorePath)(ret, "xcall/error", $(Store *, store, createStoreStringValue)(error->str));
+			g_string_free(error, true);
+			return ret;
+		}
+
+		args[i] = strdup(entry->content.string);
+	}
+
+	GString *output = $(GString *, exec, executeShellCommandArgs)(args);
+	$(bool, store, setStorePath)(ret, "output", $(Store *, store, createStoreStringValue)(output->str));
+
+	g_strfreev(args);
 
 	return ret;
 }

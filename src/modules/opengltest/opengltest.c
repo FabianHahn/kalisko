e4bc878f3b17f36101e8af9aa170c0c2a6c1ed87
Fabian Hahn <fabian@kalisko.org>
2010-09-17 21:42:13 +0200

    * added key and mouse listeners to opengltest  * opengltest version bump to 0.1.1

--- src/modules/opengltest/opengltest.c
+++ src/modules/opengltest/opengltest.c
@@ -25,26 +25,77 @@
 #include "log.h"
 #include "types.h"
 #include "modules/opengl/opengl.h"
+#include "modules/event/event.h"
 
 #include "api.h"
 
 MODULE_NAME("opengltest");
 MODULE_AUTHOR("The Kalisko team");
 MODULE_DESCRIPTION("The opengltest module creates a simple OpenGL window sample");
-MODULE_VERSION(0, 1, 0);
+MODULE_VERSION(0, 1, 1);
 MODULE_BCVERSION(0, 1, 0);
-MODULE_DEPENDS(MODULE_DEPENDENCY("opengl", 0, 2, 0), MODULE_DEPENDENCY("event", 0, 2, 1));
+MODULE_DEPENDS(MODULE_DEPENDENCY("opengl", 0, 3, 0), MODULE_DEPENDENCY("event", 0, 2, 1));
 
 static OpenGLWindow *window;
+
+static void listener_mouseDown(void *subject, const char *event, void *data, va_list args);
+static void listener_mouseUp(void *subject, const char *event, void *data, va_list args);
+static void listener_keyDown(void *subject, const char *event, void *data, va_list args);
+static void listener_keyUp(void *subject, const char *event, void *data, va_list args);
 
 MODULE_INIT
 {
 	window = $(window, opengl, createOpenGLWindow)("Kalisko OpenGL test");
+	$(void, event, attachEventListener)(window, "mouseDown", NULL, &listener_mouseDown);
+	$(void, event, attachEventListener)(window, "mouseUp", NULL, &listener_mouseUp);
+	$(void, event, attachEventListener)(window, "keyDown", NULL, &listener_keyDown);
+	$(void, event, attachEventListener)(window, "keyUp", NULL, &listener_keyUp);
 
 	return window != NULL;
 }
 
 MODULE_FINALIZE
 {
+	$(void, event, detachEventListener)(window, "mouseDown", NULL, &listener_mouseDown);
+	$(void, event, detachEventListener)(window, "mouseUp", NULL, &listener_mouseUp);
+	$(void, event, detachEventListener)(window, "keyDown", NULL, &listener_keyDown);
+	$(void, event, detachEventListener)(window, "keyUp", NULL, &listener_keyUp);
 	$(void, opengl, freeOpenGLWindow)(window);
 }
+
+static void listener_mouseDown(void *subject, const char *event, void *data, va_list args)
+{
+	int button = va_arg(args, int);
+	int x = va_arg(args, int);
+	int y = va_arg(args, int);
+
+	LOG_DEBUG("Mouse button %d down at %d/%d", button, x, y);
+}
+
+static void listener_mouseUp(void *subject, const char *event, void *data, va_list args)
+{
+	int button = va_arg(args, int);
+	int x = va_arg(args, int);
+	int y = va_arg(args, int);
+
+	LOG_DEBUG("Mouse button %d up at %d/%d", button, x, y);
+}
+
+static void listener_keyDown(void *subject, const char *event, void *data, va_list args)
+{
+	int key = va_arg(args, int);
+	int x = va_arg(args, int);
+	int y = va_arg(args, int);
+
+	LOG_DEBUG("Key '%c' down at %d/%d", key, x, y);
+}
+
+static void listener_keyUp(void *subject, const char *event, void *data, va_list args)
+{
+	int key = va_arg(args, int);
+	int x = va_arg(args, int);
+	int y = va_arg(args, int);
+
+	LOG_DEBUG("Key '%c' up at %d/%d", key, x, y);
+}
+

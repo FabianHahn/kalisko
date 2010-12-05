b8e3bc22a4f3b9341ada802ff2d9cbca68c7e858
Fabian Hahn <fabian@kalisko.org>
2010-12-05 18:43:16 +0100

    * added license to shaders in opengltest  * opengltest version bump to 0.8.3

--- src/modules/opengltest/opengltest.c
+++ src/modules/opengltest/opengltest.c
@@ -44,7 +44,7 @@
 MODULE_NAME("opengltest");
 MODULE_AUTHOR("The Kalisko team");
 MODULE_DESCRIPTION("The opengltest module creates a simple OpenGL window sample");
-MODULE_VERSION(0, 8, 2);
+MODULE_VERSION(0, 8, 3);
 MODULE_BCVERSION(0, 1, 0);
 MODULE_DEPENDS(MODULE_DEPENDENCY("freeglut", 0, 1, 0), MODULE_DEPENDENCY("opengl", 0, 8, 7), MODULE_DEPENDENCY("event", 0, 2, 1), MODULE_DEPENDENCY("module_util", 0, 1, 2), MODULE_DEPENDENCY("linalg", 0, 2, 3));
 

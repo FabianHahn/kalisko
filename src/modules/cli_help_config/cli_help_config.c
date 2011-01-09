ec0c848e24cc12cf72332960b39684331e991773
Gregor Wegberg <gregor@kalisko.org>
2011-01-09 10:28:02 +0100

    Added CLI help module for config module

---
+++ src/modules/cli_help_config/cli_help_config.c
@@ -0,0 +1,49 @@
+/**
+ * @file
+ * <h3>Copyright</h3>
+ * Copyright (c) 2011, Kalisko Project Leaders
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ *     @li Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *     @li Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer
+ *       in the documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "dll.h"
+#include "types.h"
+#include "modules/cli_help/cli_help.h"
+#include "modules/module_util/module_util.h"
+
+#include "api.h"
+
+
+MODULE_NAME("cli_help_config");
+MODULE_AUTHOR("The Kalisko team");
+MODULE_DESCRIPTION("CLI Help for the module_config module.");
+MODULE_VERSION(0, 0, 1);
+MODULE_BCVERSION(0, 0, 1);
+MODULE_DEPENDS(MODULE_DEPENDENCY("cli_help", 0, 1, 0), MODULE_DEPENDENCY("module_util", 0, 1, 0));
+
+MODULE_INIT
+{
+	$(bool, cli_help, addCLOptionHelp)("config", "c", "config", "File path to a config file. Standard config files will be not loaded (except the writable one).");
+
+	$(void, module_util, safeRevokeModule)("cli_help_config");
+
+
+	return true;
+}
+
+MODULE_FINALIZE
+{
+
+}

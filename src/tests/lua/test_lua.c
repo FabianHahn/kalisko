8ca973d37f45af9f58c8cb28c96ee3c19b4cf2d8
Fabian Hahn <fabian@kalisko.org>
2011-02-01 21:37:45 +0100

    * renamed lua test suite to test_lua * fixed lua test suite dependency on lua * lua test suite version bump to 0.4.2

--- src/tests/lua/test_lua.c
+++ src/tests/lua/test_lua.c
@@ -26,16 +26,16 @@
 #include "modules/store/parse.h"
 #include "modules/store/path.h"
 #include "modules/xcall/xcall.h"
-#include "modules/lang_lua/lang_lua.h"
+#include "modules/lua/module_lua.h"
 
 #include "api.h"
 
-MODULE_NAME("test_lang_lua");
+MODULE_NAME("test_lua");
 MODULE_AUTHOR("The Kalisko team");
-MODULE_DESCRIPTION("Test suite for the lang_lua module");
-MODULE_VERSION(0, 4, 1);
-MODULE_BCVERSION(0, 4, 1);
-MODULE_DEPENDS(MODULE_DEPENDENCY("lang_lua", 0, 7, 1), MODULE_DEPENDENCY("xcall", 0, 2, 2), MODULE_DEPENDENCY("store", 0, 6, 3));
+MODULE_DESCRIPTION("Test suite for the lua module");
+MODULE_VERSION(0, 4, 2);
+MODULE_BCVERSION(0, 4, 2);
+MODULE_DEPENDS(MODULE_DEPENDENCY("lua", 0, 8, 0), MODULE_DEPENDENCY("xcall", 0, 2, 2), MODULE_DEPENDENCY("store", 0, 6, 3));
 
 TEST_CASE(lua2store);
 TEST_CASE(store2lua);
@@ -46,7 +46,7 @@ TEST_CASE(xcall_define);
 TEST_CASE(xcall_define_error);
 TEST_CASE(xcall_direct_call);
 
-TEST_SUITE_BEGIN(lang_lua)
+TEST_SUITE_BEGIN(lua)
 	TEST_CASE_ADD(lua2store);
 	TEST_CASE_ADD(store2lua);
 	TEST_CASE_ADD(store2lua_rootlist);
@@ -60,30 +60,30 @@ TEST_SUITE_END
 TEST_CASE(lua2store)
 {
 	char *check;
-	TEST_ASSERT($(bool, lang_lua, evaluateLua)("store = parseStore('bird = word; array = { key = value; list = (1 1 2 3 5 7 13 21) }')"));
+	TEST_ASSERT($(bool, lua, evaluateLua)("store = parseStore('bird = word; array = { key = value; list = (1 1 2 3 5 7 13 21) }')"));
 
-	TEST_ASSERT($(bool, lang_lua, evaluateLua)("return store.bird"));
-	check = $(char *, lang_lua, popLuaString)();
+	TEST_ASSERT($(bool, lua, evaluateLua)("return store.bird"));
+	check = $(char *, lua, popLuaString)();
 	TEST_ASSERT(g_strcmp0(check, "word") == 0);
 	free(check);
 
-	TEST_ASSERT($(bool, lang_lua, evaluateLua)("return type(store.array)"));
-	check = $(char *, lang_lua, popLuaString)();
+	TEST_ASSERT($(bool, lua, evaluateLua)("return type(store.array)"));
+	check = $(char *, lua, popLuaString)();
 	TEST_ASSERT(g_strcmp0(check, "table") == 0);
 	free(check);
 
-	TEST_ASSERT($(bool, lang_lua, evaluateLua)("return type(store.array.list)"));
-	check = $(char *, lang_lua, popLuaString)();
+	TEST_ASSERT($(bool, lua, evaluateLua)("return type(store.array.list)"));
+	check = $(char *, lua, popLuaString)();
 	TEST_ASSERT(g_strcmp0(check, "table") == 0);
 	free(check);
 
-	TEST_ASSERT($(bool, lang_lua, evaluateLua)("return # store.array.list"));
-	check = $(char *, lang_lua, popLuaString)();
+	TEST_ASSERT($(bool, lua, evaluateLua)("return # store.array.list"));
+	check = $(char *, lua, popLuaString)();
 	TEST_ASSERT(g_strcmp0(check, "8") == 0);
 	free(check);
 
-	TEST_ASSERT($(bool, lang_lua, evaluateLua)("return store.array.list[7]"));
-	check = $(char *, lang_lua, popLuaString)();
+	TEST_ASSERT($(bool, lua, evaluateLua)("return store.array.list[7]"));
+	check = $(char *, lua, popLuaString)();
 	TEST_ASSERT(g_strcmp0(check, "13") == 0);
 	free(check);
 
@@ -92,11 +92,11 @@ TEST_CASE(lua2store)
 
 TEST_CASE(store2lua)
 {
-	TEST_ASSERT($(bool, lang_lua, evaluateLua)("return {int = 17, float = 3.14, string = 'hello world', array = {foo = 'bar'}, list = {1, 1, 2, 3, 5, 8}, nolist = {4, 2, answer = 42}}"));
+	TEST_ASSERT($(bool, lua, evaluateLua)("return {int = 17, float = 3.14, string = 'hello world', array = {foo = 'bar'}, list = {1, 1, 2, 3, 5, 8}, nolist = {4, 2, answer = 42}}"));
 
 	Store *parsed;
 
-	TEST_ASSERT((parsed = $(Store *, lang_lua, popLuaStore)()) != NULL);
+	TEST_ASSERT((parsed = $(Store *, lua, popLuaStore)()) != NULL);
 
 	Store *ret;
 	TEST_ASSERT((ret = $(Store *, store, getStorePath)(parsed, "int")) != NULL);
@@ -133,11 +133,11 @@ TEST_CASE(store2lua)
 
 TEST_CASE(store2lua_rootlist)
 {
-	TEST_ASSERT($(bool, lang_lua, evaluateLua)("return {{}}"));
+	TEST_ASSERT($(bool, lua, evaluateLua)("return {{}}"));
 
 	Store *parsed;
 
-	TEST_ASSERT((parsed = $(Store *, lang_lua, popLuaStore)()) != NULL);
+	TEST_ASSERT((parsed = $(Store *, lua, popLuaStore)()) != NULL);
 	TEST_ASSERT(parsed->type == STORE_ARRAY);
 
 	Store *ret;
@@ -153,21 +153,21 @@ TEST_CASE(store2lua_rootlist)
 TEST_CASE(store2lua_fail)
 {
 	// Test non-string values and error cascading
-	TEST_ASSERT($(bool, lang_lua, evaluateLua)("function foo() return 42; end; return {{foo}}"));
-	TEST_ASSERT($(Store *, lang_lua, popLuaStore)() == NULL);
+	TEST_ASSERT($(bool, lua, evaluateLua)("function foo() return 42; end; return {{foo}}"));
+	TEST_ASSERT($(Store *, lua, popLuaStore)() == NULL);
 
 	// Test non-table argument
-	TEST_ASSERT($(bool, lang_lua, evaluateLua)("return 42"));
-	TEST_ASSERT($(Store *, lang_lua, popLuaStore)() == NULL);
+	TEST_ASSERT($(bool, lua, evaluateLua)("return 42"));
+	TEST_ASSERT($(Store *, lua, popLuaStore)() == NULL);
 
 	TEST_PASS;
 }
 
 TEST_CASE(xcall_invoke)
 {
-	TEST_ASSERT($(bool, lang_lua, evaluateLua)("return invokeXCall('xcall = { function = some_non_existing_function }');"));
+	TEST_ASSERT($(bool, lua, evaluateLua)("return invokeXCall('xcall = { function = some_non_existing_function }');"));
 
-	char *ret = $(char *, lang_lua, popLuaString)();
+	char *ret = $(char *, lua, popLuaString)();
 
 	Store *retstore = $(Store *, store, parseStoreString)(ret);
 
@@ -183,8 +183,8 @@ TEST_CASE(xcall_invoke)
 
 TEST_CASE(xcall_define)
 {
-	TEST_ASSERT($(bool, lang_lua, evaluateLua)("function f(x) return 'bird = word' end"));
-	TEST_ASSERT($(bool, lang_lua, evaluateLua)("addXCallFunction('luatest', f)"));
+	TEST_ASSERT($(bool, lua, evaluateLua)("function f(x) return 'bird = word' end"));
+	TEST_ASSERT($(bool, lua, evaluateLua)("addXCallFunction('luatest', f)"));
 
 	Store *retstore = $(Store *, xcall, invokeXCallByString)("xcall = { function = luatest }");
 
@@ -195,16 +195,16 @@ TEST_CASE(xcall_define)
 
 	$(void, store, freeStore)(retstore);
 
-	TEST_ASSERT($(bool, lang_lua, evaluateLua)("delXCallFunction('luatest')"));
+	TEST_ASSERT($(bool, lua, evaluateLua)("delXCallFunction('luatest')"));
 
 	TEST_PASS;
 }
 
 TEST_CASE(xcall_define_error)
 {
-	TEST_ASSERT($(bool, lang_lua, evaluateLua)("function g() return 42 end"));
-	TEST_ASSERT($(bool, lang_lua, evaluateLua)("function f(x) return g end"));
-	TEST_ASSERT($(bool, lang_lua, evaluateLua)("addXCallFunction('luatest', f)"));
+	TEST_ASSERT($(bool, lua, evaluateLua)("function g() return 42 end"));
+	TEST_ASSERT($(bool, lua, evaluateLua)("function f(x) return g end"));
+	TEST_ASSERT($(bool, lua, evaluateLua)("addXCallFunction('luatest', f)"));
 
 	Store *retstore = $(Store *, xcall, invokeXCallByString)("xcall = { function = luatest }");
 
@@ -214,23 +214,23 @@ TEST_CASE(xcall_define_error)
 
 	$(void, store, freeStore)(retstore);
 
-	TEST_ASSERT($(bool, lang_lua, evaluateLua)("delXCallFunction('luatest')"));
+	TEST_ASSERT($(bool, lua, evaluateLua)("delXCallFunction('luatest')"));
 
 	TEST_PASS;
 }
 
 TEST_CASE(xcall_direct_call)
 {
-	TEST_ASSERT($(bool, lang_lua, evaluateLua)("function f() return 'ret=42' end"));
-	TEST_ASSERT($(bool, lang_lua, evaluateLua)("addXCallFunction('luatest', f)"));
-	TEST_ASSERT($(bool, lang_lua, evaluateLua)("return luatest().ret"));
+	TEST_ASSERT($(bool, lua, evaluateLua)("function f() return 'ret=42' end"));
+	TEST_ASSERT($(bool, lua, evaluateLua)("addXCallFunction('luatest', f)"));
+	TEST_ASSERT($(bool, lua, evaluateLua)("return luatest().ret"));
 
 	char *ret;
-	TEST_ASSERT((ret = $(char *, lang_lua, popLuaString)()) != NULL);
+	TEST_ASSERT((ret = $(char *, lua, popLuaString)()) != NULL);
 	TEST_ASSERT(g_strcmp0(ret, "42") == 0);
 	free(ret);
 
-	TEST_ASSERT($(bool, lang_lua, evaluateLua)("delXCallFunction('luatest')"));
+	TEST_ASSERT($(bool, lua, evaluateLua)("delXCallFunction('luatest')"));
 
 	TEST_PASS;
 }

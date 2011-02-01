760b48b4bc1e8394b2f37f29f23cadb62ccf3967
Fabian Hahn <fabian@kalisko.org>
2011-02-01 21:33:08 +0100

    * fixed deprecated lua API calls in ircpp_lua * ircpp_lua version bump to 0.2.8

--- src/modules/ircpp_lua/ircpp_lua.c
+++ src/modules/ircpp_lua/ircpp_lua.c
@@ -35,7 +35,7 @@
 MODULE_NAME("ircpp_lua");
 MODULE_AUTHOR("The Kalisko team");
 MODULE_DESCRIPTION("An IRC proxy plugin that allows proxy clients to evaluate Lua code by sending private messages to a virtual *lua bot");
-MODULE_VERSION(0, 2, 7);
+MODULE_VERSION(0, 2, 8);
 MODULE_BCVERSION(0, 1, 0);
 MODULE_DEPENDS(MODULE_DEPENDENCY("irc_proxy", 0, 3, 7), MODULE_DEPENDENCY("irc_proxy_plugin", 0, 2, 0), MODULE_DEPENDENCY("lua", 0, 8, 0), MODULE_DEPENDENCY("irc_parser", 0, 1, 1), MODULE_DEPENDENCY("string_util", 0, 1, 2), MODULE_DEPENDENCY("event", 0, 1, 2));
 
@@ -72,15 +72,15 @@ static void listener_clientLine(void *subject, const char *event, void *data, va
 
 	if($(bool, irc_proxy_plugin, isIrcProxyPluginEnabled)(client->proxy, "lua")) { // plugin is enabled for this proxy
 		if(g_strcmp0(message->command, "PRIVMSG") == 0 && message->params_count > 0 && g_strcmp0(message->params[0], "*lua") == 0 && message->trailing != NULL) { // there is a lua command to evaluate
-			if(!$(bool, lang_lua, evaluateLua)(message->trailing)) {
+			if(!$(bool, lua, evaluateLua)(message->trailing)) {
 				GString *err = g_string_new("Lua error: ");
-				char *ret = $(char *, lang_lua, popLuaString)();
+				char *ret = $(char *, lua, popLuaString)();
 				g_string_append(err, ret);
 				$(bool, irc_proxy, proxyClientIrcSend)(client, ":*lua!kalisko@kalisko.proxy PRIVMSG %s :%s", client->proxy->irc->nick, ret);
 				free(ret);
 				g_string_free(err, true);
 			} else {
-				char *ret = $(char *, lang_lua, popLuaString)();
+				char *ret = $(char *, lua, popLuaString)();
 
 				if(ret != NULL) {
 					$(void, string_util, stripDuplicateNewlines)(ret);

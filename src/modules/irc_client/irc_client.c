f4d05bfffeaa08b1b1362279908f1e49f641122d
Fabian Hahn <fabian@kalisko.org>
2011-02-08 11:11:55 +0100

    added irc_client module

---
+++ src/modules/irc_client/irc_client.c
@@ -0,0 +1,82 @@
+/**
+ * @file
+ * <h3>Copyright</h3>
+ * Copyright (c) 2011, Kalisko Project Leaders
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ *     @li Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *     @li Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer
+ *       in the documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <assert.h>
+#include <gtk/gtk.h>
+
+#include "dll.h"
+#include "modules/gtk+/gtk+.h"
+#include "modules/gtk+/builder.h"
+#include "modules/config/config.h"
+#include "modules/store/store.h"
+#include "modules/store/path.h"
+#include "api.h"
+
+MODULE_NAME("irc_client");
+MODULE_AUTHOR("The Kalisko team");
+MODULE_DESCRIPTION("A graphical IRC client using GTK+");
+MODULE_VERSION(0, 1, 0);
+MODULE_BCVERSION(0, 1, 0);
+MODULE_DEPENDS(MODULE_DEPENDENCY("gtk+", 0, 2, 6), MODULE_DEPENDENCY("store", 0, 6, 10), MODULE_DEPENDENCY("config", 0, 3, 9));
+
+static void finalize();
+
+/**
+ * The window widget for the IRC client
+ */
+static GtkWidget *window;
+
+MODULE_INIT
+{
+	GString *path = g_string_new($$(char *, getExecutablePath)());
+	g_string_append(path, "/modules/irc_client/irc_client.xml");
+
+	GtkBuilder *builder = $(GtkBuilder *, gtk+, loadGtkBuilderGui)(path->str);
+
+	g_string_free(path, true);
+
+	if(builder == NULL) {
+		LOG_ERROR("Failed to load IRC client GUI");
+		return false;
+	}
+
+	window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
+
+	// window
+	gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
+
+	// show everything
+	gtk_widget_show_all(GTK_WIDGET(window));
+
+	// run
+	$(void, gtk+, runGtkLoop)();
+
+	return true;
+}
+
+MODULE_FINALIZE
+{
+	finalize();
+}
+
+static void finalize()
+{
+	gtk_widget_destroy(GTK_WIDGET(window));
+}

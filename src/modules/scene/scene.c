45af83b9a197fd655764d41c9c2d975542fc42fe
Fabian Hahn <fabian@kalisko.org>
2011-02-24 12:12:45 +0100

    * fixed scene dependency on opengl * scene version bump to 0.2.11

--- src/modules/scene/scene.c
+++ src/modules/scene/scene.c
@@ -43,9 +43,9 @@
 MODULE_NAME("scene");
 MODULE_AUTHOR("The Kalisko team");
 MODULE_DESCRIPTION("The scene module represents a loadable OpenGL scene that can be displayed and interaced with");
-MODULE_VERSION(0, 2, 10);
+MODULE_VERSION(0, 2, 11);
 MODULE_BCVERSION(0, 1, 0);
-MODULE_DEPENDS(MODULE_DEPENDENCY("opengl", 0, 11, 7), MODULE_DEPENDENCY("linalg", 0, 3, 0), MODULE_DEPENDENCY("mesh", 0, 4, 0), MODULE_DEPENDENCY("store", 0, 6, 10));
+MODULE_DEPENDS(MODULE_DEPENDENCY("opengl", 0, 13, 0), MODULE_DEPENDENCY("linalg", 0, 3, 0), MODULE_DEPENDENCY("mesh", 0, 4, 0), MODULE_DEPENDENCY("store", 0, 6, 10));
 
 static void freeOpenGLMeshByPointer(void *mesh_p);
 static void freeSceneParameterByPointer(void *parameter_p);

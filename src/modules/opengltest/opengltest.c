604184527f9c346456a7826fb2c8fe74b882c746
Fabian Hahn <fabian@kalisko.org>
2011-02-24 17:30:41 +0100

    * swapped opengltest materials for models * opengltest version bump to 0.12.1

--- src/modules/opengltest/opengltest.c
+++ src/modules/opengltest/opengltest.c
@@ -47,7 +47,7 @@
 MODULE_NAME("opengltest");
 MODULE_AUTHOR("The Kalisko team");
 MODULE_DESCRIPTION("The opengltest module creates a simple OpenGL window sample");
-MODULE_VERSION(0, 12, 0);
+MODULE_VERSION(0, 12, 1);
 MODULE_BCVERSION(0, 1, 0);
 MODULE_DEPENDS(MODULE_DEPENDENCY("freeglut", 0, 1, 0), MODULE_DEPENDENCY("opengl", 0, 13, 0), MODULE_DEPENDENCY("event", 0, 2, 1), MODULE_DEPENDENCY("module_util", 0, 1, 2), MODULE_DEPENDENCY("linalg", 0, 3, 3), MODULE_DEPENDENCY("mesh", 0, 4, 0), MODULE_DEPENDENCY("scene", 0, 2, 3), MODULE_DEPENDENCY("image_png", 0, 1, 2));
 

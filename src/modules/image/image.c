ebc4f3ab33299046f37eab2bd4d6f77700973f19
Fabian Hahn <fabian@kalisko.org>
2011-06-06 20:27:35 +0200

    added getImageAsByte function to image

--- src/modules/image/image.c
+++ src/modules/image/image.c
@@ -28,7 +28,7 @@
 MODULE_NAME("image");
 MODULE_AUTHOR("The Kalisko team");
 MODULE_DESCRIPTION("Module providing a general image data type");
-MODULE_VERSION(0, 5, 6);
+MODULE_VERSION(0, 5, 7);
 MODULE_BCVERSION(0, 5, 0);
 MODULE_DEPENDS(MODULE_DEPENDENCY("store", 0, 6, 10));
 

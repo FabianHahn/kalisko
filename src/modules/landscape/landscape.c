c5f1f6c178e0f3822d08c68a798c798291afadc4
Simon Wenner <simon@kalisko.org>
2011-06-11 15:53:48 +0200

    landscape: use worley noise, add overall strategy

--- src/modules/landscape/landscape.c
+++ src/modules/landscape/landscape.c
@@ -22,11 +22,13 @@
 #include <assert.h>
 #include <glib.h>
 #include <stdio.h>
+#include <limits.h>
 #include "dll.h"
 #include "modules/erosion/erosion.h"
 #include "modules/image/image.h"
 #include "modules/image/io.h"
 #include "modules/random/perlin.h"
+#include "modules/random/worley.h"
 #include "modules/scene/primitive.h"
 #include "api.h"
 #include "landscape.h"
@@ -35,9 +37,9 @@
 MODULE_NAME("landscape");
 MODULE_AUTHOR("The Kalisko team");
 MODULE_DESCRIPTION("Module to display randomly generated landscapes");
-MODULE_VERSION(0, 1, 5);
+MODULE_VERSION(0, 1, 6);
 MODULE_BCVERSION(0, 1, 0);
-MODULE_DEPENDS(MODULE_DEPENDENCY("heightmap", 0, 2, 6), MODULE_DEPENDENCY("store", 0, 6, 11), MODULE_DEPENDENCY("opengl", 0, 27, 0), MODULE_DEPENDENCY("scene", 0, 5, 2), MODULE_DEPENDENCY("image", 0, 5, 0), MODULE_DEPENDENCY("random", 0, 4, 1), MODULE_DEPENDENCY("erosion", 0, 1, 2), MODULE_DEPENDENCY("image_png", 0, 1, 2));
+MODULE_DEPENDS(MODULE_DEPENDENCY("heightmap", 0, 2, 6), MODULE_DEPENDENCY("store", 0, 6, 11), MODULE_DEPENDENCY("opengl", 0, 27, 0), MODULE_DEPENDENCY("scene", 0, 5, 2), MODULE_DEPENDENCY("image", 0, 5, 0), MODULE_DEPENDENCY("random", 0, 4, 1), MODULE_DEPENDENCY("erosion", 0, 1, 2), MODULE_DEPENDENCY("image_png", 0, 1, 2), MODULE_DEPENDENCY("image_pnm", 0, 2, 5));
 
 MODULE_INIT
 {
@@ -59,19 +61,54 @@ MODULE_FINALIZE
  */
 API Image *generateLandscapeHeightmap(unsigned int width, unsigned int height, double frequency)
 {
-#if 1
-	Image *map = $(Image *, image, createImageFloat)(width, height, 1);
-	float maxValue = 0.0, minValue = 0.0;
+#define DEBUGIMAGES
+	// inefficent but useful for debugging
+	Image *map, *worley, *fBm;
+	float maxValue = 0.0, minValue = FLT_MAX;
 
+	map =		$(Image *, image, createImageFloat)(width, height, 1);
+	worley = 	$(Image *, image, createImageFloat)(width, height, 1);
+	fBm =		$(Image *, image, createImageFloat)(width, height, 1);
+
+	// 1. create worley noise for the overall map structure (valleys, peaks and ridges)
+	RandomWorleyCtx* worleyCtx = createWorleyCtx(20, 2);
 	for(unsigned int y = 0; y < height; y++) {
 		for(unsigned int x = 0; x < width; x++) {
-			//setImage(map, x, y, 0, 0.5 + $(float, random, randomPerlin)((double) x * frequency / width, (double) y * frequency / height, 0.0));
+			float value = $(float, random, randomWorley)(worleyCtx,
+					(double) x / width, (double) y / height, 0.0,
+					1, RANDOM_DIST_EUCLIDEAN);
+			setImage(worley, x, y, 0, value);
 
+			if(value > maxValue)
+				maxValue = value;
+			if(value < minValue)
+				minValue = value;
+		}
+	}
+	freeWorleyCtx(worleyCtx);
+
+	// rescale heightmap to [0,1] and invert it
+	for(unsigned int y = 0; y < height; y++) {
+		for(unsigned int x = 0; x < width; x++) {
+			float value = (getImage(worley, x, y, 0) - minValue) / (maxValue - minValue);
+			setImage(worley, x, y, 0, 1.f - value);
+		}
+	}
+
+#ifdef DEBUGIMAGES
+	assert($(bool, image, writeImageToFile)("worley.pgm", worley));
+#endif
+	maxValue = 0.0;
+	minValue = 0.0;
+
+	// 2. create fBm noise to get interresting features of different frequencies
+	for(unsigned int y = 0; y < height; y++) {
+		for(unsigned int x = 0; x < width; x++) {
 			float value = 0.5 + $(float, random, noiseFBm)(x, y, 0.0,
 					frequency / width, frequency / height, 0.0,
 					/* persistance */ 0.5,
 					/* depth */ 6);
-			setImage(map, x, y, 0, value);
+			setImage(fBm, x, y, 0, value);
 
 			if(value > maxValue)
 				maxValue = value;
@@ -83,10 +120,22 @@ API Image *generateLandscapeHeightmap(unsigned int width, unsigned int height, d
 	// rescale heightmap to [0,1]
 	for(unsigned int y = 0; y < height; y++) {
 		for(unsigned int x = 0; x < width; x++) {
-			setImage(map, x, y, 0, (getImage(map, x, y, 0) - minValue) / (maxValue - minValue));
+			setImage(fBm, x, y, 0, (getImage(fBm, x, y, 0) - minValue) / (maxValue - minValue));
 		}
 	}
-#else
+
+#ifdef DEBUGIMAGES
+	assert($(bool, image, writeImageToFile)("fbm.pgm", fBm));
+#endif
+
+	// 3. filter the worley noise to remove straight lines
+	// TODO
+
+	// 4. combine worley noise and fBm
+	// TODO
+
+	// 5. apply erosion to make the appearance physically-based
+#if 0
 	char *execpath = $$(char *, getExecutablePath)();
 	GString *path = g_string_new(execpath);
 	g_string_append(path, "/modules/erosion/erosion_in.png");
@@ -99,5 +148,7 @@ API Image *generateLandscapeHeightmap(unsigned int width, unsigned int height, d
 	//erodeThermal(map, M_PI/4.5, 50); // 40 deg
 	//erodeHydraulic(map, 100);
 
-	return map;
+
+	// 6. profit!
+	return fBm; // FIXME
 }

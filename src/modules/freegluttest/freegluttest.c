54667f130c96c156bf88b94f27578320fb0abaf1
Fabian Hahn <fabian@kalisko.org>
2011-06-13 13:37:21 +0200

    made freegluttest a lot less verbose

--- src/modules/freegluttest/freegluttest.c
+++ src/modules/freegluttest/freegluttest.c
@@ -47,7 +47,7 @@
 MODULE_NAME("freegluttest");
 MODULE_AUTHOR("The Kalisko team");
 MODULE_DESCRIPTION("The freegluttest module creates a simple OpenGL window sample using freeglut");
-MODULE_VERSION(0, 14, 18);
+MODULE_VERSION(0, 14, 19);
 MODULE_BCVERSION(0, 1, 0);
 MODULE_DEPENDS(MODULE_DEPENDENCY("freeglut", 0, 1, 0), MODULE_DEPENDENCY("opengl", 0, 27, 0), MODULE_DEPENDENCY("event", 0, 2, 1), MODULE_DEPENDENCY("module_util", 0, 1, 2), MODULE_DEPENDENCY("linalg", 0, 3, 3), MODULE_DEPENDENCY("scene", 0, 5, 2), MODULE_DEPENDENCY("image_png", 0, 1, 2), MODULE_DEPENDENCY("mesh_opengl", 0, 2, 0), MODULE_DEPENDENCY("particle", 0, 6, 6), MODULE_DEPENDENCY("heightmap", 0, 1, 0), MODULE_DEPENDENCY("landscape", 0, 1, 0));
 
@@ -61,8 +61,6 @@ static int currentHeight = 600;
 static bool cameraTiltEnabled = false;
 static float rotation = 0.0f;
 
-static void listener_mouseDown(void *subject, const char *event, void *data, va_list args);
-static void listener_mouseUp(void *subject, const char *event, void *data, va_list args);
 static void listener_keyDown(void *subject, const char *event, void *data, va_list args);
 static void listener_keyUp(void *subject, const char *event, void *data, va_list args);
 static void listener_display(void *subject, const char *event, void *data, va_list args);
@@ -141,8 +139,6 @@ MODULE_INIT
 		return false;
 	}
 
-	$(void, event, attachEventListener)(window, "mouseDown", NULL, &listener_mouseDown);
-	$(void, event, attachEventListener)(window, "mouseUp", NULL, &listener_mouseUp);
 	$(void, event, attachEventListener)(window, "keyDown", NULL, &listener_keyDown);
 	$(void, event, attachEventListener)(window, "keyUp", NULL, &listener_keyUp);
 	$(void, event, attachEventListener)(window, "display", NULL, &listener_display);
@@ -157,8 +153,6 @@ MODULE_INIT
 
 MODULE_FINALIZE
 {
-	$(void, event, detachEventListener)(window, "mouseDown", NULL, &listener_mouseDown);
-	$(void, event, detachEventListener)(window, "mouseUp", NULL, &listener_mouseUp);
 	$(void, event, detachEventListener)(window, "keyDown", NULL, &listener_keyDown);
 	$(void, event, detachEventListener)(window, "keyUp", NULL, &listener_keyUp);
 	$(void, event, detachEventListener)(window, "display", NULL, &listener_display);
@@ -173,32 +167,9 @@ MODULE_FINALIZE
 	$(void, opengl, freeOpenGLCamera)(camera);
 	$(void, linalg, freeMatrix)(perspectiveMatrix);
 }
-
-static void listener_mouseDown(void *subject, const char *event, void *data, va_list args)
-{
-	int button = va_arg(args, int);
-	int x = va_arg(args, int);
-	int y = va_arg(args, int);
-
-	LOG_DEBUG("Mouse button %d down at %d/%d", button, x, y);
-}
-
-static void listener_mouseUp(void *subject, const char *event, void *data, va_list args)
-{
-	int button = va_arg(args, int);
-	int x = va_arg(args, int);
-	int y = va_arg(args, int);
-
-	LOG_DEBUG("Mouse button %d up at %d/%d", button, x, y);
-}
-
 static void listener_keyDown(void *subject, const char *event, void *data, va_list args)
 {
 	int key = va_arg(args, int);
-	int x = va_arg(args, int);
-	int y = va_arg(args, int);
-
-	LOG_DEBUG("Key '%c' down at %d/%d", key, x, y);
 
 	if(key >= 0 && key < 256) {
 		keysPressed[key] = true;
@@ -217,10 +188,6 @@ static void listener_keyDown(void *subject, const char *event, void *data, va_li
 static void listener_keyUp(void *subject, const char *event, void *data, va_list args)
 {
 	int key = va_arg(args, int);
-	int x = va_arg(args, int);
-	int y = va_arg(args, int);
-
-	LOG_DEBUG("Key '%c' up at %d/%d", key, x, y);
 
 	if(key >= 0 && key < 256) {
 		keysPressed[key] = false;

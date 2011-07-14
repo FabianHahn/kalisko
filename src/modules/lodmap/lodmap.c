e64322d58acfc9651969faf2cdaea7a330d56702
Fabian Hahn <fabian@kalisko.org>
2011-07-14 18:46:25 +0200

    implemented lodmap upsampling

--- src/modules/lodmap/lodmap.c
+++ src/modules/lodmap/lodmap.c
@@ -34,9 +34,9 @@
 MODULE_NAME("lodmap");
 MODULE_AUTHOR("The Kalisko team");
 MODULE_DESCRIPTION("Module for OpenGL level-of-detail maps");
-MODULE_VERSION(0, 1, 2);
+MODULE_VERSION(0, 1, 3);
 MODULE_BCVERSION(0, 1, 0);
-MODULE_DEPENDS(MODULE_DEPENDENCY("opengl", 0, 27, 0), MODULE_DEPENDENCY("heightmap", 0, 2, 13), MODULE_DEPENDENCY("quadtree", 0, 6, 7), MODULE_DEPENDENCY("image", 0, 5, 14));
+MODULE_DEPENDS(MODULE_DEPENDENCY("opengl", 0, 27, 0), MODULE_DEPENDENCY("heightmap", 0, 2, 13), MODULE_DEPENDENCY("quadtree", 0, 7, 0), MODULE_DEPENDENCY("image", 0, 5, 14));
 
 static void *loadLodMapTile(Quadtree *tree, QuadtreeNode *node);
 static void freeLodMapTile(Quadtree *tree, void *data);
@@ -66,7 +66,7 @@ MODULE_FINALIZE
 API OpenGLPrimitive *createOpenGLPrimitiveLodMap(unsigned int leafSize, const char *dataPrefix, const char *dataSuffix)
 {
 	OpenGLLodMap *lodmap = ALLOCATE_OBJECT(OpenGLLodMap);
-	lodmap->quadtree = $(Quadtree *, quadtree, createQuadtree)(leafSize, 25, &loadLodMapTile, &freeLodMapTile);
+	lodmap->quadtree = $(Quadtree *, quadtree, createQuadtree)(leafSize, 25, &loadLodMapTile, &freeLodMapTile, true);
 	lodmap->dataPrefix = strdup(dataPrefix);
 	lodmap->dataSuffix = strdup(dataSuffix);
 	lodmap->primitive.type = "lodmap";
@@ -114,25 +114,56 @@ static void *loadLodMapTile(Quadtree *tree, QuadtreeNode *node)
 	OpenGLLodMap *lodmap = g_hash_table_lookup(maps, tree);
 	assert(lodmap != NULL);
 
-	GString *path = g_string_new(lodmap->dataPrefix);
-	g_string_append_printf(path, "%hd.%hd.%hu.%s", node->x, node->x, node->level, lodmap->dataSuffix);
-
 	OpenGLLodMapTile *tile = ALLOCATE_OBJECT(OpenGLLodMapTile);
-	tile->heights = $(Image *, image, readImageFromFile)(path->str);
-	g_string_free(path, true);
 
-	if(tile->heights != NULL && (tile->heights->width != tree->leafSize || tile->heights->height != tree->leafSize)) { // invalid map, we need to recover
-		$(void, image, freeImage)(tile->heights);
-		tile->heights = NULL;
-	}
+	if(node->level == 0) { // load the lowest level from disk
+		GString *path = g_string_new(lodmap->dataPrefix);
+		g_string_append_printf(path, "%hd.%hd.%hu.%s", node->x, node->x, node->level, lodmap->dataSuffix);
+		tile->heights = $(Image *, image, readImageFromFile)(path->str);
+		g_string_free(path, true);
 
-	if(tile->heights == NULL) { // failed to load, we need to recover
+		if(tile->heights != NULL && (tile->heights->width != tree->leafSize || tile->heights->height != tree->leafSize)) { // invalid map, we need to recover
+			$(void, image, freeImage)(tile->heights);
+			tile->heights = NULL;
+		}
+
+		if(tile->heights == NULL) { // failed to load, we need to recover
+			tile->heights = $(Image *, image, createImageFloat)(tree->leafSize, tree->leafSize, 1);
+			$(void, image, clearImage)(tile->heights);
+		}
+	} else { // construct higher LOD levels by downsampling the lower detailed ones
+		// these should be preloaded
+		assert(quadtreeNodeDataIsLoaded(node->children[0]));
+		assert(quadtreeNodeDataIsLoaded(node->children[1]));
+		assert(quadtreeNodeDataIsLoaded(node->children[2]));
+		assert(quadtreeNodeDataIsLoaded(node->children[3]));
+
+		// create the image to which we upsample
 		tile->heights = $(Image *, image, createImageFloat)(tree->leafSize, tree->leafSize, 1);
-		$(void, image, clearImage)(tile->heights);
+
+		// upsample the heights
+		unsigned int halfsize = tree->leafSize / 2;
+		for(unsigned int y = 0; y < tree->leafSize; y++) {
+			bool isLowerY = y < halfsize;
+			int offsetY = isLowerY ? 0 : -halfsize;
+
+			for(unsigned int x = 0; x < tree->leafSize; x++) {
+				bool isLowerX = x < halfsize;
+				int offsetX = isLowerX ? 0 : -halfsize;
+
+				// determine correct sub image
+				unsigned int index = (isLowerX ? 0 : 1) + (isLowerY ? 0 : 2);
+				Image *subheights = ((OpenGLLodMapTile *) node->children[index]->data)->heights;
+
+				// upsample the height
+				float value = 0.25f * (getImage(subheights, offsetX + 2 * x, offsetY + 2 * y, 0) + getImage(subheights, offsetX + 2 * x + 1, offsetY + 2 * y, 0) + getImage(subheights, offsetX + 2 * x, offsetY + 2 * y + 1, 0) + getImage(subheights, offsetX + 2 * x + 1, offsetY + 2 * y + 1, 0));
+				setImage(tile->heights, x, y, 0, value);
+			}
+		}
 	}
 
+	// Compute normals
 	tile->normals = $(Image *, image, createImageFloat)(tree->leafSize, tree->leafSize, 3);
-
 	$(void, heightmap, computeHeightmapNormals)(tile->heights, tile->normals);
 
 	return tile;

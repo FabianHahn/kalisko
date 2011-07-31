7a8f1d4f2b80b87b88423cb41cedca28134746f6
Fabian Hahn <fabian@kalisko.org>
2011-07-31 13:13:29 +0200

    fixed lodmap compilation on Windows

--- src/modules/lodmap/lodmap.c
+++ src/modules/lodmap/lodmap.c
@@ -37,7 +37,7 @@
 MODULE_NAME("lodmap");
 MODULE_AUTHOR("The Kalisko team");
 MODULE_DESCRIPTION("Module for OpenGL level-of-detail maps");
-MODULE_VERSION(0, 4, 1);
+MODULE_VERSION(0, 4, 2);
 MODULE_BCVERSION(0, 2, 0);
 MODULE_DEPENDS(MODULE_DEPENDENCY("opengl", 0, 29, 4), MODULE_DEPENDENCY("heightmap", 0, 3, 2), MODULE_DEPENDENCY("quadtree", 0, 7, 15), MODULE_DEPENDENCY("image", 0, 5, 16), MODULE_DEPENDENCY("image_pnm", 0, 2, 6), MODULE_DEPENDENCY("image_png", 0, 1, 4), MODULE_DEPENDENCY("linalg", 0, 3, 4));
 
@@ -85,7 +85,7 @@ API OpenGLLodMap *createOpenGLLodMap(double baseRange, unsigned int viewingDista
 
 	// create lodmap material
 	$(bool, opengl, deleteOpenGLMaterial)("lodmap");
-	char *execpath = $$(char *, getExecutablePath());
+	char *execpath = $$(char *, getExecutablePath)();
 	GString *vertexShaderPath = g_string_new(execpath);
 	g_string_append_printf(vertexShaderPath, "/modules/lodmap/lodmap.glslv");
 	GString *fragmentShaderPath = g_string_new(execpath);

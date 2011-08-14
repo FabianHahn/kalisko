d73296a53ea08b06fb8ff39fd7b0971a30c6f869
Fabian Hahn <fabian@kalisko.org>
2011-08-14 17:47:00 +0200

    fixed border texture propagation in lodmap

--- src/modules/lodmap/lodmap.c
+++ src/modules/lodmap/lodmap.c
@@ -39,7 +39,7 @@
 MODULE_NAME("lodmap");
 MODULE_AUTHOR("The Kalisko team");
 MODULE_DESCRIPTION("Module for OpenGL level-of-detail maps");
-MODULE_VERSION(0, 8, 7);
+MODULE_VERSION(0, 8, 8);
 MODULE_BCVERSION(0, 8, 0);
 MODULE_DEPENDS(MODULE_DEPENDENCY("opengl", 0, 29, 6), MODULE_DEPENDENCY("heightmap", 0, 4, 0), MODULE_DEPENDENCY("quadtree", 0, 9, 0), MODULE_DEPENDENCY("image", 0, 5, 16), MODULE_DEPENDENCY("image_pnm", 0, 2, 6), MODULE_DEPENDENCY("image_png", 0, 1, 4), MODULE_DEPENDENCY("linalg", 0, 3, 4));
 
@@ -388,12 +388,6 @@ static void *loadLodMapTile(Quadtree *tree, QuadtreeNode *node)
 					if(propagateNormals) {
 						for(unsigned int c = 0; c < 3; c++) {
 							setImage(tile->normals, x, y, c, 0.0);
-						}
-					}
-
-					if(propagateTexture) {
-						for(unsigned int c = 0; c < 3; c++) {
-							setImage(tile->texture, x, y, c, 0.0);
 						}
 					}
 

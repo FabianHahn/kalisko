9c0e798d6ea3c258824e4a326cd4ba651407891c
Dino Wernli <dino@kalisko.org>
2013-01-06 15:00:59 +0100

    * added missing copyright notice

--- src/tests/http_server/http_server.c
+++ src/tests/http_server/http_server.c
@@ -1,7 +1,8 @@
 /**
  * @file
  * <h3>Copyright</h3>
- * Copyright (c) 2009, Kalisko Project Leaders
+ * Copyright (c) 2012, Kalisko Project Leaders
+ * Copyright (c) 2012, Google Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:

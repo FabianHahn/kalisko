6d985783f56f5e3f16fde345e4b64c31d115bef9
Dino Wernli <dino@kalisko.org>
2013-01-20 13:23:04 +0100

    refactored the testing facilities: * test fixtures (setup and teardown) are now supported * passing a test is implicit * metadata about tests is stored to allow filtering or prettier output * API is currently backwards compatible, the next is to migrate existing tests and rename a few functions for consistency

--- src/test.c
+++ src/test.c
@@ -18,23 +18,37 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include "test.h"
+
 #include <glib.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define API
+#include <string.h> // strdup
+
+#include "dll.h"
 #include "log.h"
-#include "module.h"
-#include "types.h"
-#include "timer.h"
-#include "util.h"
-#include "test.h"
 #include "memory_alloc.h"
+#include "module.h"
+#include "timer.h"
+#include "types.h"
+#include "util.h"
+#define API
 
-static int test = 0;
-static int passed = 0;
-static int count = 0;
+#define TERMINAL_WIDTH 80
 
-#define BUF 4096
+// TODO: Use the logging facilities instead of printf for these
+#define TEST_OUTPUT_INFO(...) printf(__VA_ARGS__); printf("\n");
+#define TEST_OUTPUT_ERROR(...) fprintf(stderr, __VA_ARGS__); printf("\n");
+
+static unsigned int tests_passed = 0;
+static unsigned int tests_ran = 0;
+
+static void appendRight(GString *message, char *suffix);
+static TestFixture *createTestFixture(char *name, SetupFunction *setup, TeardownFunction *teardown);
+static void destroyTestFixture(TestFixture *fixture);
+static TestCase *createTestCase(char *name, TestFunction *function, TestFixture *fixture);
+static void destroyTestCase(TestCase *test_case);
+static bool testCaseFailed(TestCase *test_case);
 
 int main(int argc, char **argv)
 {
@@ -45,7 +59,7 @@ int main(int argc, char **argv)
 	initLog();
 	initModules();
 
-	printf("Running test cases...\n");
+	TEST_OUTPUT_INFO("Running test cases...\n");
 
 	char *execpath = getExecutablePath();
 	char *testdir = g_build_path("/", execpath, "tests", NULL);
@@ -54,7 +68,7 @@ int main(int argc, char **argv)
 	GDir *tests = g_dir_open(testdir, 0, &error);
 
 	if(tests == NULL) {
-		fprintf(stderr, "Error: Could not open tests dir: %s\n", error != NULL ? error->message : "no error message");
+		TEST_OUTPUT_ERROR("Error: Could not open tests dir: %s\n", error != NULL ? error->message : "no error message");
 		g_error_free(error);
 		return EXIT_FAILURE;
 	}
@@ -66,10 +80,8 @@ int main(int argc, char **argv)
 
 		if(g_file_test(entry, G_FILE_TEST_IS_DIR)) {
 			char *modname = g_strjoin(NULL, "test_", node, NULL);
-
 			if(!requestModule(modname)) {
-				test = 0;
-				reportTestResult(modname, "load", false, "Failed to load module");
+				TEST_OUTPUT_ERROR("Error: Failed to load test module: %s", modname);
 			} else {
 				revokeModule(modname);
 			}
@@ -87,40 +99,146 @@ int main(int argc, char **argv)
 
 	freeModules();
 
-	if(count) {
-		double perc = 100.0 * passed / count;
-		printf("\n%d of %d test cases passed (%.2f%%)\n", passed, count, perc);
+	if(tests_ran > 0) {
+		double perc = 100.0 * tests_passed / tests_ran;
+		TEST_OUTPUT_INFO("\n%d of %d test cases passed (%.2f%%)", tests_passed, tests_ran, perc);
 	}
 
 	return EXIT_SUCCESS;
 }
 
-API void incTestCount()
+API TestSuite *createTestSuite(char *name)
 {
-	test++;
+	TestSuite *result = ALLOCATE_OBJECT(TestSuite);
+	result->name = strdup(name);
+	result->test_cases = g_ptr_array_new_with_free_func((GDestroyNotify) &destroyTestCase);
+	result->test_fixtures = g_hash_table_new_full(g_str_hash, g_str_equal, free, (GDestroyNotify) &destroyTestFixture);
+	return result;
 }
 
-API void resetTestCount()
+API void destroyTestSuite(TestSuite *test_suite)
 {
-	test = 0;
+	free(test_suite->name);
+	g_ptr_array_free(test_suite->test_cases, true);   // Frees(destroys) the contained structs
+	g_hash_table_destroy(test_suite->test_fixtures);  // Frees(destroys) the contained structs
+	free(test_suite);
 }
 
-API void reportTestResult(char *testsuite, char *testcase, bool pass, char *error, ...)
+API void addTestFixture(TestSuite *test_suite, char *name, SetupFunction *setup, TeardownFunction *teardown)
 {
-	if(pass) {
-		passed++;
-		printf("[%s] %s: Passed\n", testsuite, testcase);
-	} else {
-		va_list va;
-		char buffer[BUF];
+	assert(test_suite->test_fixtures != NULL);
+	TestFixture *test_fixture = createTestFixture(name, setup, teardown);
+	g_hash_table_replace(test_suite->test_fixtures, strdup(name), test_fixture);
+}
 
-		va_start(va, error);
-		vsnprintf(buffer, BUF, error, va);
-
-		printf("[%s] %s: Failed test #%d (%s)\n", testsuite, testcase, test, buffer);
+API void addTest(TestSuite *test_suite, char *name, TestFunction *test_function, char *fixture_name)
+{
+	assert(test_suite->test_fixtures != NULL);
+	assert(test_suite->test_cases != NULL);
+	TestFixture *test_fixture = NULL;
+	if(fixture_name != NULL) {
+		test_fixture = g_hash_table_lookup(test_suite->test_fixtures, fixture_name);
 	}
+	TestCase *test_case = createTestCase(name, test_function, test_fixture);
+	g_ptr_array_add(test_suite->test_cases, test_case);
+}
 
-	fflush(stdout);
+API void runTestSuite(TestSuite *test_suite)
+{
+	GPtrArray *cases = test_suite->test_cases;
+	for(int i = 0; i < cases->len; ++i) {
+		TestCase *test_case = g_ptr_array_index(cases, i);
 
-	count++;
+		GString *message = g_string_new("");
+		g_string_append_printf(message, "Test case [%s] %s:", test_suite->name, test_case->name);
+
+		TestFixture *fixture = test_case->test_fixture;
+		if(fixture != NULL && fixture->setup_function != NULL) {
+			fixture->setup_function();
+		}
+		test_case->test_function(test_case);
+		if(fixture != NULL && fixture->teardown_function !=NULL) {
+			fixture->teardown_function();
+		}
+
+		if(testCaseFailed(test_case)) {
+			appendRight(message, "FAIL");
+			g_string_append_printf(message, "\n  Error: %s", test_case->error);
+		} else {
+			tests_passed++;
+			appendRight(message, "PASS");
+		}
+
+		tests_ran++;
+
+		TEST_OUTPUT_INFO("%s", message->str);
+		g_string_free(message, true);
+	}
+}
+
+API void failTest(TestCase *test_case, char* error, ...)
+{
+	free(test_case->error);
+	va_list va;	
+	test_case->error = g_strdup_vprintf(error, va);
+}
+
+/**
+ * Appends spaces and the provided suffix to message such that the suffix is
+ * right-aligned in terminal output. If impossible, this just appends the suffix
+ */
+static void appendRight(GString *message, char *suffix)
+{
+	int free_space = TERMINAL_WIDTH - (strlen(suffix) + message->len);
+	if (free_space < 0) {
+		// No room on this line, just append (and it won't look pretty)
+		g_string_append(message, suffix);
+		return;
+	}
+	char *whitespace = g_strnfill(free_space, ' ');
+	g_string_append_printf(message, "%s%s", whitespace, suffix);
+	free(whitespace);
+}
+
+static TestFixture *createTestFixture(char *name, SetupFunction *setup, TeardownFunction *teardown)
+{
+	TestFixture *result = ALLOCATE_OBJECT(TestFixture);
+	result->name = strdup(name);
+	result->setup_function = setup;
+	result->teardown_function = teardown;
+	return result;
+}
+
+static void destroyTestFixture(TestFixture *test_fixture)
+{
+	if(test_fixture == NULL) {
+		return;
+	}
+	free(test_fixture->name);
+	free(test_fixture);
+}
+
+static TestCase *createTestCase(char *name, TestFunction *function, TestFixture *fixture)
+{
+	TestCase *result = ALLOCATE_OBJECT(TestCase);
+	result->name = strdup(name);
+	result->test_function = function;
+	result->test_fixture = fixture;
+	result->error = NULL;
+	return result;
+}
+
+static void destroyTestCase(TestCase *test_case)
+{
+	if(test_case == NULL) {
+		return;
+	}
+	free(test_case->name);
+	free(test_case->error);
+	free(test_case);
+}
+
+static bool testCaseFailed(TestCase *test_case)
+{
+	return test_case->error != NULL;
 }
--- src/test.i
+++ src/test.i
@@ -2,6 +2,7 @@
  * @file
  * <h3>Copyright</h3>
  * Copyright (c) 2009, Kalisko Project Leaders
+ * Copyright (c) 2012, Google Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -24,76 +25,182 @@
 #include "module.h"
 #include "types.h"
 
+// Forward declaration used for function types
+struct TestCaseStruct;
 
 /**
- * Increment the test count
+ * A function which can be used as setup in a test fixture
  */
-API void incTestCount();
+typedef void (SetupFunction)();
 
 /**
- * Resets the test count
+ * A function which can be used as teardown in a test fixture
  */
-API void resetTestCount();
+typedef void (TeardownFunction)();
 
 /**
- * Reports a test result of a test case
+ * A function which can be used as a test case
+ */
+typedef void (TestFunction)(struct TestCaseStruct*);
+
+/**
+ * Represents an environment for unit tests in which setup is called
+ * before each test and teardown is called after each test
+ */
+typedef struct
+{
+	/** Identifies this fixture */
+	char *name;
+	/** Optional setup function */
+	SetupFunction *setup_function;
+	/** Optional teardown function */
+	TeardownFunction *teardown_function;
+} TestFixture;
+
+/**
+ * Represents a basic unit test which can pass or fail
+ */
+typedef struct TestCaseStruct
+{
+	/** Identifies this test */
+	char *name;
+	/** Stores the function which is run as a test */
+	TestFunction *test_function;
+	/** Optional fixture for setup and teardown */
+	TestFixture *test_fixture;
+	/** Optional error describing what went wrong */
+	char *error;
+} TestCase;
+
+/**
+ * Represents a collection of unit tests. Groups them to be
+ * run in sequence
+ */
+typedef struct
+{
+	/** Identifies this test suite */
+	char *name;
+	/** Stores the test cases for in-order execution */
+	GPtrArray *test_cases;
+	/** Stores all used fixtures by name */
+	GHashTable *test_fixtures;
+} TestSuite;
+
+/**
+ * Constructor for the TestSuite struct. The caller is responsible for
+ * eventually calling destroyTestSuite
  *
- * @param testsuite		the name of the test suite
- * @param testcase		the name of the test case
- * @param pass			did the test pass?
- * @param error			if it didn't pass, printf-like error message
+ * @param name			the name of the new test suite, will be used
+ *						to identify the test suite in test output
+ * @result				the new TestSuite struct
  */
-API void reportTestResult(char *testsuite, char *testcase, bool pass, char *error, ...);
+API TestSuite *createTestSuite(char *name);
+
+/**
+ * Frees all allocated memory associated with a TestSuite
+ *
+ * @param test_suite 	the TestSuite to be destroyed
+ */
+API void destroyTestSuite(TestSuite *test_suite);
+
+/**
+ * Makes a test fixture available in a test suite. After this call, tests
+ * can make use of the new fixture
+ *
+ * @param test_suite 	the TestSuite to which a fixture is added
+ * @param name 			the name of the fixture
+ * @param setup			a setup function for the fixture
+ * @param teardown		a teardown function for the fixture
+ */
+API void addTestFixture(TestSuite *test_suite, char *name, SetupFunction *setup, TeardownFunction *teardown);
+
+/**
+ * Adds a new test to a test suite. If no fixture with the provided name
+ * exists (or the fixture name if NULL), adds a test without a fixture
+ *
+ * @param test_suite	the test suite to add the test to
+ * @param name			the name of the test
+ * @param function		a test function to be executed when running the test
+ * @param fixture_name	the name of a fixture registered with this test suite
+ */
+API void addTest(TestSuite *test_suite, char *name, TestFunction *function, char *fixture_name);
+
+/**
+ * Executes all tests in the test suite.
+ */
+API void runTestSuite(TestSuite *test_suite);
+
+/**
+ * Fails the given test case and stores the error message
+ *
+ * @param error			printf-style reason for the error
+ */
+API void failTest(TestCase *test_case, char *error, ...);
+
+/**
+ * Defines a new test case
+ * TODO: Rename to TEST (current name for migration purposes)
+ *
+ * @param CASE			the test case's name
+ */
+#define TEST_CASE(TEST_NAME) static void TEST_NAME(TestCase *test_case)
+
+/**
+ * Adds a new fixture to the current test module
+ *
+ * @param FIXTURE_NAME	name identifying the fixture
+ * @param SETUP			optional setup function called before each test
+ * @param TEARDOWN		optional teardown function called after each test
+ */
+#define ADD_TEST_FIXTURE(FIXTURE_NAME, SETUP, TEARDOWN) addTestFixture(test_suite, #FIXTURE_NAME, SETUP, TEARDOWN);
+
+/**
+ * Adds a test case without fixture to the current test suite
+ * TODO: rename to ADD_SIMPLE_TEST (current name for migration purposes)
+ *
+ * @param TEST_NAME		the name of the test to add
+ */
+#define TEST_CASE_ADD(TEST_NAME) addTest(test_suite, #TEST_NAME, TEST_NAME, NULL);
+
+/**
+ * Adds a test case with a fixture to the current test suite.
+ *
+ * @param TEST_NAME		the name of the test to add
+ * @param FIXTURE_NAME	the name of the fixture to use for this test
+ */
+#define ADD_FIXTURED_TEST(TEST_NAME, FIXTURE_NAME) addTest(test_suite, #TEST_NAME, TEST_NAME, #FIXTURE_NAME);
 
 /**
  * Initializes a test suite
  *
  * @param SUITE		the name of the test suite
  */
-#define TEST_SUITE_BEGIN(SUITE) MODULE_INIT \
+#define TEST_SUITE_BEGIN(SUITE_NAME) MODULE_INIT \
 	{ \
-		char *testsuite = #SUITE;
+		TestSuite *test_suite = createTestSuite(#SUITE_NAME);
 
-#define TEST_SUITE_END return true; \
+/**
+ * Cleans up the test suite
+ */
+#define TEST_SUITE_END runTestSuite(test_suite); destroyTestSuite(test_suite); return true; \
 	}
-
-/**
- * Returns the internal function name of a test case
- *
- * @param CASE		the test case's name
- */
-#define TEST_CASE_NAME(CASE) _test_case_ ## CASE
-
-/**
- * Adds a test case to a test suite
- *
- * @param CASE		the test case's name
- */
-#define TEST_CASE_ADD(CASE) $$(void, resetTestCount)(); TEST_CASE_NAME(CASE)(testsuite, #CASE)
-
-/**
- * Defines a test case
- *
- * @param CASE		the test case's name
- */
-#define TEST_CASE(CASE) static void TEST_CASE_NAME(CASE)(char *testsuite, char *testcase)
 
 /**
  * Checks if an expression holds, and fails the test case if it doesn't
  *
  * @param EXPR		an expression to check
  */
-#define TEST_ASSERT(EXPR) if(!(EXPR)) { TEST_FAIL("Line %d: Assertion failed: " #EXPR, __LINE__); } else { $$(void, incTestCount)(); }
-
-/**
- * Passes a test case
- */
-#define TEST_PASS $$(void, reportTestResult)(testsuite, testcase, true, NULL); return
+#define TEST_ASSERT(EXPR) if(!(EXPR)) { TEST_FAIL("Line %d: Assertion failed: " #EXPR, __LINE__); }
 
 /**
  * Fails a test case with an error message
  *
  * @param ERROR		printf-like error message
  */
-#define TEST_FAIL(ERROR, ...) $$(void, incTestCount)(); $$(void, reportTestResult)(testsuite, testcase, false, ERROR, ##__VA_ARGS__); return
+#define TEST_FAIL(ERROR, ...) failTest(test_case, ERROR, ##__VA_ARGS__); return;
+
+// This has no effect. Passing a test is implicit. Define preserved for legacy code
+// TODO: Migrate all code using this and remove it
+#define TEST_PASS ;
+
 #endif
